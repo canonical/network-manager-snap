a48e97ee37116de230fdc5e514622b4dcf66c5b5
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2019-01-14 17:33:25 +0100

    Fix some shellcheck warnings

--- snap-common/startup-hooks/99-wol-by-default.sh
+++ snap-common/startup-hooks/99-wol-by-default.sh
@@ -2,9 +2,9 @@
 
 # Enable wake-on-lan by default until we have a configuration
 # hook to do that.
-if [ ! -e $SNAP_DATA/conf.d/enable-wol.conf ] ; then
-	mkdir -p $SNAP_DATA/conf.d
-	cat <<-EOF > $SNAP_DATA/conf.d/enable-wol.conf
+if [ ! -e "$SNAP_DATA"/conf.d/enable-wol.conf ] ; then
+	mkdir -p "$SNAP_DATA"/conf.d
+	cat <<-EOF > "$SNAP_DATA"/conf.d/enable-wol.conf
 		[connection]
 		# Value 64 maps to the 'magic' setting; see man nm-settings
 		# for more information.
