dfe30061b1dad7b00e6ca80a6f118291f98639f1
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2022-02-01 18:07:09 +0100

    tests: check that we remove netplan configuration on removal

--- tests/lib/restore-each.sh
+++ tests/lib/restore-each.sh
@@ -23,13 +23,16 @@ done
 rm -f /etc/netplan/*
 
 sleep 2
-systemctl stop snap.network-manager.networkmanager
+# Make sure stopping the service does not fail as maybe we
+# have removed the snap in the test.
+systemctl stop snap.network-manager.networkmanager || true
 rm -rf /var/snap/network-manager/*
 tar xzf "$SPREAD_PATH"/nm-state.tar.gz -C /
 # Wait a bit to avoid hitting re-start limit
 sleep 2
 
-snap install --dangerous $PROJECT_PATH/*_amd64.snap
+snap_install network-manager --channel=20/stable
+
 sleep 2
 
 # Make sure the original netplan configuration is applied and active
