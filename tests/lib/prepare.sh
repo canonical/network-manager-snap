c33e652fcc08f08d7a011ae1830f490f3bf277c7
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2022-02-02 14:40:24 +0100

    tests: reinstall NM after each test
    This makes restoring the state much simpler. Also, the new remove hook
    helps here to have things cleaned up. A couple of tests had to be
    modified to make sure they restored the state properly.

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -22,23 +22,7 @@ for name in "$gadget_name" "$kernel_name" "$core_name"; do
 	fi
 done
 
-# Remove any existing state archive from other test suites
-rm -f /home/network-manager/nm-state.tar.gz
-
 snap_install network-manager --channel=20/stable
-
-# snapshot NetworkManager's state
-sleep 2
-systemctl stop snap.network-manager.networkmanager
-tar czf "$SPREAD_PATH"/nm-state.tar.gz /var/snap/network-manager /etc/netplan
-# Wait a bit to avoid hitting re-start limit
-sleep 2
-# Make sure the original netplan configuration is applied and active
-# (we do this before re-starting NM to avoid race conditions in some tests)
-netplan generate
-# Remove ipv6 addresses (see LP:#1870561)
-ip -6 address flush dev "$eth_if"
-systemctl start snap.network-manager.networkmanager
 wait_for_network_manager
 
 # For debugging dump all snaps and connected slots/plugs
--- tests/lib/restore-each.sh
+++ tests/lib/restore-each.sh
@@ -18,28 +18,8 @@ for snap in /snap/*; do
 	esac
 done
 
-# Drop any generated or modified netplan configuration files. The original
-# ones will be restored below.
-rm -f /etc/netplan/*
-
-sleep 2
-# Make sure stopping the service does not fail as maybe we
-# have removed the snap in the test.
-systemctl stop snap.network-manager.networkmanager || true
-rm -rf /var/snap/network-manager/*
-tar xzf "$SPREAD_PATH"/nm-state.tar.gz -C /
-# Wait a bit to avoid hitting re-start limit
-sleep 2
-
+if snap list | grep "$SNAP_NAME" ; then
+	snap remove --purge network-manager
+fi
 snap_install network-manager --channel=20/stable
-
-sleep 2
-
-# Make sure the original netplan configuration is applied and active
-# (we do this before re-starting NM to avoid race conditions in some tests)
-netplan apply
-# Remove ipv6 addresses (see LP:#1870561)
-ip -6 address flush dev "$eth_if"
-
-systemctl start snap.network-manager.networkmanager
 wait_for_network_manager
