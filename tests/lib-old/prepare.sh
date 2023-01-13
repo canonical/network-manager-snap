286c8f3fa003284b606bd2211b1d5913a59e1adf
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2023-01-13 08:11:52 +0000

    Tests run for the first time

--- tests/lib-old/prepare.sh
+++ tests/lib-old/prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 # shellcheck source=tests/lib/utilities.sh
-. "$TESTSLIB"/utilities.sh
+. "$TESTSLIBOLD"/utilities.sh
 get_qemu_eth_iface eth_if
 
 echo "Wait for firstboot change to be ready"
@@ -12,7 +12,7 @@ done
 
 echo "Ensure fundamental snaps are still present"
 # shellcheck source=tests/lib/snap-names.sh
-. "$TESTSLIB"/snap-names.sh
+. "$TESTSLIBOLD"/snap-names.sh
 for name in "$gadget_name" "$kernel_name" "$core_name"; do
 	if ! snap list | grep -q "$name" ; then
 		echo "Not all fundamental snaps are available, all-snap image not valid"
